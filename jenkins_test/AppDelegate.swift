b7fcd7bd214d82b798dc4087788cbaabc1fb5352
tomaskrasnay <tomas.krasnay@adastra.one>
2018-03-23 14:09:43 +0100

    1. test

--- jenkins_test/AppDelegate.swift
+++ jenkins_test/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import HockeySDK
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,7 +17,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        self.hockeyAppConfig()
         return true
+    }
+
+    private func hockeyAppConfig() {
+        BITHockeyManager.shared().configure(withIdentifier: "959ce81fe42546aab133d68a08d1fe66")
+        BITHockeyManager.shared().start()
+        BITHockeyManager.shared().authenticator.authenticateInstallation()
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
